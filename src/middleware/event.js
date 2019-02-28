a5ef92908cff596dd0ec7014237d5e99fb0cceaf
b40429 <b40429@banrisul.com.br>
2019-02-28 13:14:26 -0300

    add: static folder, chekc for mimetype and echo for getevent

--- src/middleware/event.js
+++ src/middleware/event.js
@@ -1,12 +1,13 @@
 import EventDao from "../model/event.dao";
 import fs from "fs";
 import shortid from "shortid";
+
 const roles = ["p", "a"];
 
 export default class Event {
   static async createEvent(ctx) {
     try {
-      const { event } = ctx.request.body;
+      const { event = {} } = ctx.request.body;
       const { user } = ctx.state;
 
       if (!Array.includes(roles, user.role)) {
@@ -18,16 +19,24 @@ export default class Event {
         return;
       }
 
-      if (!event.clients || event.clients.length <= 1) {
-        ctx.badRequest({ error: "Bad event format, expected a clients list." });
+      if (!(event.clients instanceof Array)) {
+        ctx.badRequest({ error: "Bad event format, expected a client´s list." });
         return;
       }
-      if (!event.photos || event.photos.lenght < 1) {
-        ctx.badRequest({ error: "Bad event format, expected a client list." });
+      if (!(event.photos instanceof Array)) {
+        ctx.badRequest({ error: "Bad event format, expected a photo´s list." });
         return;
       }
       event.status = "pending upload";
       event.hash = shortid.generate(); //hash to create a dir and save files
+      const userSlug = user.email.substr(0, user.email.indexOf("@") + 1);
+      const eventFolder = `./static/${userSlug}${event.hash}`;
+
+      event.photos = event.photos.map(photo => ({
+        ...photo,
+        path: `${eventFolder}/${photo.name}`
+      }));
+
       await EventDao.saveEvent(event, user);
 
       ctx.ok({ message: `Event ${event.name} created`, event: event });
@@ -39,8 +48,14 @@ export default class Event {
 
   static async uploadPhoto(ctx) {
     const { user } = ctx.state;
-    const { eventHash } = ctx.request.body;
+    const { eventHash, uploadError } = ctx.request.body;
     const photo = ctx.request.files[0];
+
+    
+    if (uploadError) {
+      ctx.badRequest({ error: uploadError });
+      return; 
+    }
 
     if (!Array.includes(roles, user.role)) {
       ctx.unauthorized("User does not have permissions for it.");
@@ -58,7 +73,7 @@ export default class Event {
     }
 
     const userSlug = user.email.substr(0, user.email.indexOf("@") + 1);
-    const eventFolder = `./${userSlug}${eventHash}`;
+    const eventFolder = `./static/${userSlug}${eventHash}`;
 
     !fs.existsSync(eventFolder) && fs.mkdirSync(eventFolder);
 
@@ -69,5 +84,14 @@ export default class Event {
     ctx.ok({ message: "Upload complete!", status, photo: photo.filename });
   }
 
-  static getEvent() {}
+  static getEvent(ctx) {
+    const { event = {} } = ctx.request.body;  
+
+    if (!event.code) {
+      ctx.badRequest({ error: "Bad event format, expected a event code." });
+      return;
+    }
+
+    ctx.ok({event})
+  }
 }
--- src/routes/event.js
+++ src/routes/event.js
@@ -1,9 +1,16 @@
 import Router from "koa-router";
 import Middleware from "../middleware/event";
-import busboy from 'koa-busboy';
+import busboy from "koa-busboy";
 
 const router = new Router();
+router.post("/", Middleware.getEvent);
 router.post("/create", Middleware.createEvent);
-router.post("/upload-photo", busboy(), Middleware.uploadPhoto);
+router.post(
+  "/upload-photo",
+  busboy({
+    acceptedMimeTypes: ["image/gif", "image/jpeg", "image/png", "image/svg+xml", "image/x-icon"]
+  }),
+  Middleware.uploadPhoto
+);
 
 export default router.routes();
